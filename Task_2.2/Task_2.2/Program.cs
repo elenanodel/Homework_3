149b7f1728cc3242978a18c8345881747c69a08e
User <User@WIN-4I8V0TPRSH5>
2017-08-25 13:30:12 +0300

    Init Commit

---
+++ Task_2.2/Task_2.2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, enter two numbers and we'll calculate the result by action you'll choose");
+
+            double a;
+            double b;
+            char c;
+
+            Console.Write("Enter first number: ");
+            a = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Choose an action:");
+            Console.WriteLine("'+' - addition");
+            Console.WriteLine("'-' - substraction");
+            Console.WriteLine("'*' - multiplication");
+            Console.WriteLine("'-' - division");
+            Console.WriteLine("'%' - rest of division");
+            Console.Write("Enter your choice: ");
+            c = char.Parse(Console.ReadLine());
+
+            switch (c)
+            {
+                case '+':
+                    {
+                        Console.WriteLine("You choose an addition. Reasult: " + (a + b));
+                        break;
+                    }
+                case '-':
+                    {
+                        Console.WriteLine("You choose an substraction. Result: " + (a - b));
+                        break;
+                    }
+                case '*':
+                    {
+                        Console.WriteLine("You choose an multiplication. Result: " + (a * b));
+                        break;
+                    }
+                case '/':
+                    {
+                        Console.WriteLine("You choose an division. Result: " + (a / b));
+                        break;
+                    }
+                case '%':
+                    {
+                        Console.WriteLine("You choose a res of division. Result: " + (a % b));
+                        break;
+                    }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
